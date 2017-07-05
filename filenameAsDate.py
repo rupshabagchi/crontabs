6cf3138f4cc35dd3c0b115c2e2945c90d14d7c3e
Rupsha <rupsha.bagchi@gmail.com>
2017-07-05 14:45:57 +0300

    filename as current time

--- filenameAsDate.py
+++ filenameAsDate.py
@@ -8,7 +8,7 @@ import datetime
 filename = datetime.datetime.now()
 
 def create_file():
-    with open(str(filename),"w+") as file:
+    with open(str(filename.strftime("%Y-%m-%d-%H-%M")),"w+") as file:
         file.write("")
 
 create_file()
